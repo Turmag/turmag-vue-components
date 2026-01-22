54ec777c166e5a86721498bd9f11a2ef3744b413
Pavel Vinogradov <turmagger@gmail.com>
2026-01-22 23:34:30 +0300

    feat: main Добавлен компонент UiText

---
+++ src/components/UiText.vue
@@ -0,0 +1,96 @@
+<template>
+    <span
+        :class="[
+            $style.text,
+            $style[color],
+            $style[size],
+            $style[weight],
+            $style[line],
+            $style[wrap],
+            $style[`display-${display}`],
+        ]"
+    >
+        <slot>{{ text }}</slot>
+    </span>
+</template>
+
+<script lang="ts" setup>
+import type { Ref } from 'vue';
+
+withDefaults(defineProps<{
+    color?: 'default' | 'hover' | 'active' | 'secondary';
+    display?: 'block' | 'inline' | 'flex' | '';
+    line?: 'lh100' | 'lh110' | 'lh120' | 'lh130' | 'lh140' | 'lh150';
+    size?: 'fs12' | 'fs14' | 'fs16' | 'fs18' | 'fs20' | 'fs24' | 'fs36';
+    text?: string | number | Ref<string, string>;
+    weight?: 'fw400' | 'fw500' | 'fw600' | 'fw700';
+    wrap?: 'nowrap' | 'balance' | '';
+}>(), {
+    color: 'default',
+    cursor: '',
+    display: '',
+    line: 'lh100',
+    size: 'fs16',
+    text: '',
+    weight: 'fw400',
+    wrap: '',
+});
+</script>
+
+<style module lang="scss">
+    .text {
+        line-height: 100%;
+    }
+
+    .block {
+        display: block;
+    }
+
+    $text-colors: default, hover, active, secondary;
+
+    @each $color in $text-colors {
+        .#{$color} {
+            color: var(--color-text-#{$color});
+        }
+    }
+
+    $sizes: 12, 14, 16, 18, 20, 24, 36;
+
+    @each $size in $sizes {
+        .fs#{$size} {
+            font-size: #{$size}px;
+        }
+    }
+
+    $weights: 400, 500, 600, 700;
+
+    @each $weight in $weights {
+        .fw#{$weight} {
+            font-weight: #{$weight};
+        }
+    }
+
+    $line-heights: 100, 110, 120, 130, 140, 150;
+
+    @each $line-height in $line-heights {
+        .lh#{$line-height} {
+            line-height: #{$line-height}#{'%'};
+        }
+    }
+
+    $displays: block, inline, flex;
+
+    @each $display in $displays {
+        .display-#{$display} {
+            display: #{$display};
+        }
+    }
+
+    .nowrap {
+        white-space: nowrap;
+    }
+
+    .balance {
+        text-wrap: balance;
+    }
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import type { App, Plugin } from 'vue';
 import UiFlex from './components/UiFlex.vue';
+import UiText from './components/UiText.vue';
 
 // Plugin installation
 export const install = (app: App): void => {
     app.component('UiFlex', UiFlex);
+    app.component('UiText', UiText);
 };
 
 // Vue plugin
@@ -13,11 +15,12 @@ const VuComponentsPlugin: Plugin = { install };
 export default VuComponentsPlugin;
 
 // Named exports (for direct import)
-export { UiFlex };
+export { UiFlex, UiText };
 
 // Type augmentation for global components
 declare module '@vue/runtime-core' {
     export interface IGlobalComponents {
         UiFlex: typeof UiFlex;
+        UiText: typeof UiText;
     }
 }
