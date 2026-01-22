ec156ff34c8f385951635b3d46c1bbab3d561943
Pavel Vinogradov <turmagger@gmail.com>
2026-01-22 23:14:05 +0300

    feat: main В компонент UiFlex добавлена вариация для position: fixed

--- src/components/UiFlex.vue
+++ src/components/UiFlex.vue
@@ -50,7 +50,7 @@ withDefaults(defineProps<{
     padding?: '' | 'p2' | 'p4' | 'p6' | 'p8' | 'p10' | 'p12' | 'p14' | 'p16' | 'p18' | 'p20' | 'p24' | 'p28' | 'p32' | 'p36' | 'p40' | 'p42' | 'p64' | 'p80' | 'p120';
     paddingBlock?: '' | 'p2' | 'p4' | 'p6' | 'p8' | 'p10' | 'p12' | 'p14' | 'p16' | 'p18' | 'p20' | 'p24' | 'p28' | 'p32' | 'p36' | 'p40' | 'p42' | 'p64' | 'p80' | 'p120';
     paddingInline?: '' | 'p2' | 'p4' | 'p6' | 'p8' | 'p10' | 'p12' | 'p14' | 'p16' | 'p18' | 'p20' | 'p24' | 'p28' | 'p32' | 'p36' | 'p40' | 'p42' | 'p64' | 'p80' | 'p120';
-    position?: '' | 'relative' | 'absolute' | 'sticky';
+    position?: '' | 'relative' | 'absolute' | 'sticky' | 'fixed';
     radius?: '' | 'r0' | 'r2' | 'r4' | 'r6' | 'r8' | 'r12' | 'r14' | 'r16' | 'r18' | 'r20' | 'r22' | 'r24' | 'r26' | 'r28' | 'r30' | 'r32';
     rowGap?: '' | 'g2' | 'g4' | 'g6' | 'g8' | 'g10' | 'g12' | 'g16' | 'g20' | 'g24' | 'g28' | 'g32' | 'g36' | 'g40' | 'g42' | 'g48' | 'g56' | 'g60' | 'g64' | 'g80' | 'g120';
     tag?: string;
@@ -114,7 +114,7 @@ gapSizes.forEach(size => {
         width: 100%;
     }
 
-    $positions: relative, absolute, sticky;
+    $positions: relative, absolute, sticky, fixed;
 
     @each $position in $positions {
         .position-#{$position} {
